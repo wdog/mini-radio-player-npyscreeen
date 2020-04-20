990fc4bfe955dfd82e535823ca34971d08474055
wdog <wdog666@gmail.com>
2020-04-20 02:20:54 +0200

    inital release

---
+++ station.py
@@ -0,0 +1,41 @@
+import logging
+import json
+import sys
+
+
+class Station:
+    """STATION CLASS - MANAGES RADIO LIST"""
+    stations = []
+
+    def __init__(self):
+        self.load_stations_list()
+
+    def load_stations_list(self):
+        """Load radio station list"""
+        with open('radio.json') as f:
+            sts = json.load(f)
+      
+        self.stations = []
+        for s in sts:
+            self.stations.append(RadioStation(s))            
+
+    def change_order(self, src, dst):
+        """ rewrite stations file with different order """
+        origSrc = self.stations[src]
+        self.stations[src] = self.stations[dst]
+        self.stations[dst] = origSrc
+        try:  
+            with open('radio.json','w') as f:
+                json.dump(self.stations,f)
+        except Exception as e:
+            logging.warn(e)
+
+
+class RadioStation():
+    """docstring for RadioStation"""
+    def __init__(self,s):
+        self.name= s['name']
+        self.url = s['url']
+        
+    def __str__(self):
+        return self.name
